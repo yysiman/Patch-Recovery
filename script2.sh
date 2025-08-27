e578b777e1a06c1acd1b48998a0f60476e5080dd
yysiman <yysiman@gmail.com>
2025-08-27 23:47:04 +0800

    script2.sh

--- script2.sh
+++ script2.sh
@@ -16,6 +16,18 @@ cd unpack
 ../magiskboot hexpatch system/bin/recovery 26f0ceec30b1681c 26f0ceec30b9681c
 ../magiskboot hexpatch system/bin/recovery 24f0fcee30b1681c 24f0fcee30b9681c
 ../magiskboot hexpatch system/bin/recovery 27f02eeb30b1681c 27f02eeb30b9681c
+../magiskboot hexpatch system/bin/recovery b4f082ee28b1701c b4f082ee28b970c1
+../magiskboot hexpatch system/bin/recovery 9ef0f4ec28b1701c 9ef0f4ec28b9701c
+../magiskboot hexpatch system/bin/recovery 9ef00ced28b1701c 9ef00ced28b9701c
+../magiskboot hexpatch system/bin/recovery 2001597ae0000054 2001597ae1000054 # ccmp w9, w25, #0, eq ; b.e #0x20 ===> b.ne #0x20
+../magiskboot hexpatch system/bin/recovery 2001597ac0000054 2001597ac1000054 # ccmp w9, w25, #0, eq ; b.e #0x1c ===> b.ne #0x1c
+
+../magiskboot hexpatch system/bin/recovery 9ef0fcec28b1701c 9ef0fced28b1701c
+../magiskboot hexpatch system/bin/recovery 9ef00ced28b1701c 9ef00ced28b9701c
+
+../magiskboot hexpatch system/bin/recovery 24f0f2ea30b1681c 24f0f2ea30b9681c
+../magiskboot hexpatch system/bin/recovery e0031f2a8e000014 200080528e000014
+../magiskboot hexpatch system/bin/recovery 41010054a0020012f44f48a9 4101005420008052f44f48a9
 ../magiskboot  cpio ramdisk.cpio 'add 0755 system/bin/recovery system/bin/recovery'
 ../magiskboot  repack ../recovery.img new-boot.img
 cp new-boot.img ../recovery-patched.img
